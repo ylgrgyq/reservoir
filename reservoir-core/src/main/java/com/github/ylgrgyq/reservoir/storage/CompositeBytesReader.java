6986197aed05c7ee6a4a9fb4085b305d4e615ae3
ylgrgyq <alven.gr@gmail.com>
2019-09-23 09:44:25 +0800

    (chore) composite reader

--- reservoir-core/src/main/java/com/github/ylgrgyq/reservoir/storage/CompositeBytesReader.java
+++ reservoir-core/src/main/java/com/github/ylgrgyq/reservoir/storage/CompositeBytesReader.java
@@ -3,93 +3,55 @@ package com.github.ylgrgyq.reservoir.storage;
 import java.nio.*;
 import java.util.List;
 
-class CompositeByteBufferReader {
+class CompositeBytesReader {
     private final List<byte[]> buckets;
-    private int index;
+    private int bucketIndex;
+    private int offset;
 
-    public CompositeByteBufferReader(List<byte[] buckets) {
+    public CompositeBytesReader(List<byte[]> buckets) {
         this.buckets = buckets;
     }
 
     private int getBucketIndex() {
-        return index / buckets.size();
-    }
-
-    private int getOffset() {
-        
+        return bucketIndex / buckets.size();
     }
 
     public byte get() {
-        return 0;
+        byte[] bucket;
+        for (bucket = buckets.get(bucketIndex); offset >= bucket.length; ){
+            offset = 0;
+            ++bucketIndex;
+        }
+
+        return bucket[offset++];
     }
 
-
-    public byte get(int index) {
-        return 0;
-    }
-
-
-    public ByteBuffer compact() {
-        return null;
-    }
-
-
-    public char getChar() {
-        return 0;
-    }
-
-
-    public char getChar(int index) {
-        return 0;
-    }
-
+//    public char getChar() {
+//        byte[] bucket;
+//        for (bucket = buckets.get(bucketIndex); offset >= bucket.length; ){
+//            offset = 0;
+//            ++bucketIndex;
+//        }
+//        return bucket[offset++];
+//    }
 
     public short getShort() {
         return 0;
     }
 
-
-    public short getShort(int index) {
-        return 0;
-    }
-
-
     public int getInt() {
         return 0;
     }
-
-
-    public int getInt(int index) {
-        return 0;
-    }
-
 
     public long getLong() {
         return 0;
     }
 
-    public long getLong(int index) {
-        return 0;
-    }
-
-
     public float getFloat() {
         return 0;
     }
 
-
-    public float getFloat(int index) {
-        return 0;
-    }
-
-
     public double getDouble() {
         return 0;
     }
-
-
-    public double getDouble(int index) {
-        return 0;
-    }
-
 }
